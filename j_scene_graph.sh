7ee08e0bf9138dd6c0cce28c2158d1f4ee38c217
Ethan-Chiu <ethanchiu0608@gmail.com>
2026-04-08 04:47:09 -0400

    run scene graph job

--- j_scene_graph.sh
+++ j_scene_graph.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=download
+#SBATCH --job-name=scenegraphextract
 #SBATCH --partition=general
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:L40S:1
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 #SBATCH --output=/home/ethanchi/data-gen-log/scene_graphs/slurm-%j.log
 #SBATCH --error=/home/ethanchi/data-gen-log/scene_graphs/slurm-%j.err
 #SBATCH --mail-type=END
@@ -14,14 +14,15 @@
 
 echo "Running scene graph extraction..."
 
-export NUM_SHARDS=4
-export SLURM_ARRAY_TASK_ID=0
+export NUM_SHARDS=3
+export SLURM_ARRAY_TASK_ID=2
 
 uv run python scripts/extract_scene_graphs.py \
   --config configs/semantic_10k.toml \
   --shard-id "$SLURM_ARRAY_TASK_ID" \
   --num-shards "$NUM_SHARDS" \
-  --concurrency 1  2>&1
+  --concurrency 1 \
+  --limit 15000 2>&1
 
 
 echo "Job finished."
